016da37399abf60661532d5ab2143ebfc4662ce1
Dimitar Manastirsky <dmanastirski@gmail.com>
2024-10-16 10:50:36 +0200

    further adjustments to icon, font, menu fadein fadeout

--- scripts/header.js
+++ scripts/header.js
@@ -1,19 +1,34 @@
 const menuToggle = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.menu')
+const logo = document.querySelector('.logo')
+const link = document.querySelector('.menu li')
+const underline = document.querySelector('underline');
 
 // GSAP Start Animation
-
 gsap.from('.menu-toggle', { duration: 0.7 , opacity: 0, scale: 0, y: '-100%' })
 
-gsap.from('.logo', { duration: 1 , opacity: 0, scale: 0, y: '-100%' })
+gsap.from('.logo', { duration: 1 , opacity: 0, scale: 0})
 
-// Toggle Script
-menuToggle.addEventListener('click', () => {
+// Toggle
+menuToggle.addEventListener('click', (event) => {
+    event.preventDefault();
+
     menuToggle.classList.toggle('active')
     menu.classList.toggle('active')
 })
 
-// Logo Hover
+// Hover
+logo.addEventListener('mouseover', () =>{
+    let pulse = document.createElement('div');
 
-let logo = document.querySelector('.logo')
+    pulse.classList.add('pulse');
+    logo.appendChild(pulse);
 
+    setTimeout(function () {
+        pulse.remove();
+    }, 1000);
+})
+
+link.addEventListener('mouseover', () =>{
+    
+})
