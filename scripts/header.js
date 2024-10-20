90e2655a1ed2bf373a53eed0de25ced7e820951b
Dimitar Manastirsky <dmanastirski@gmail.com>
2024-10-20 18:21:48 +0200

    added start animations to text and github icon/added gsap code to scripts

--- scripts/header.js
+++ scripts/header.js
@@ -8,6 +8,16 @@ gsap.from('.menu-toggle', { duration: 0.7 , opacity: 0, scale: 0, y: '-100%' })
 
 gsap.from('.logo', { duration: 1 , opacity: 0, scale: 0})
 
+gsap.from('.welcome-container h3', { duration: 1.5, opacity: 0, x: '-100%', delay: 0.5})
+gsap.from('.welcome-container h2', { duration: 1.5, opacity: 0, x: '-100%', delay: 0.8})
+gsap.from('.welcome-container h1', { duration: 1.5, opacity: 0, x: '-100%', delay: 1.2})
+gsap.from('.welcome-container a ', { duration: 1.5, opacity: 0, delay: 2.5})
+
+gsap.from('.team-container h3', { duration: 1.5, opacity: 0, x: '100%', delay: 1.7})
+gsap.from('.team-container h1', { duration: 1.5, opacity: 0, x: '100%', delay: 1.9})
+
+
+
 // Toggle
 menuToggle.addEventListener('click', (event) => {
     event.preventDefault();
